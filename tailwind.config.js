6128cc620d18f1634d34487aed5d5a2401eba911
sonic <labibferdous14@gmail.com>
2026-09-11 03:03:25 +0600

    update tailwind js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'brand-gradient': 'linear-gradient(to right, #f97316, #ec4899, #8b5cf6)',
+      }
+    },
   },
   plugins: [],
 }
-
